3b4097f3044d409a56acb82aeea811a6b2a39913
vmbansal2001 <vmbansal2001@gmail.com>
2026-01-29 11:59:59 +0530

    feat: add redirects for project pages in Next.js configuration
    - Implemented redirects for specific project pages to enhance user navigation and SEO.
    - Added permanent redirects from "/mount-ville" to "/project/mount-ville", "/casabella" to "/project/casa-bella", and "/royaleville" to "/project/royale-ville".

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,25 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/mount-ville",
+        destination: "/project/mount-ville",
+        permanent: true,
+      },
+      {
+        source: "/casabella",
+        destination: "/project/casa-bella",
+        permanent: true,
+      },
+      {
+        source: "/royaleville",
+        destination: "/project/royale-ville",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const svgrConfig = withSVGR as (config: NextConfig) => NextConfig;

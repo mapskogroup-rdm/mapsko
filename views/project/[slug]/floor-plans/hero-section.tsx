7e67c5d70bc41cab9997d45c616565eaf47e5721
RDM <Web@reddashmedia.com>
2026-07-16 15:17:59 +0530

    Update floor plans heading for The Icon 79

--- views/project/[slug]/floor-plans/hero-section.tsx
+++ views/project/[slug]/floor-plans/hero-section.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         <div className="w-screen h-[500px] flex items-center justify-between flex-col text-white px-4 sm:px-6">
           <PropertyNavbar property={property} />
           <h1 className="text-center justify-start text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-light">
-            Floor Plans
+            {property.name === "The Icon 79" ? "The Icon 79 Floor Plans" : "Floor Plans"}
           </h1>
           <div className="h-16" />
         </div>
